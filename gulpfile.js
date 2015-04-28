96bdf28fd620d4b6c1d68f2e33ba7722741a74bb
Rouan <rmw@thoughtworks.com>
2015-04-28 17:32:06 +0200

    add a clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var del = require('del');
 
-gulp.task('concat', function () {
+gulp.task('clean', function () {
+  return del(['css', 'dist'], function (err, paths) {
+    console.log('Deleted files/folders:\n', paths.join('\n'));
+  });
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('concat', ['sass'], function () {
   return gulp.src('./css/*.css')
     .pipe(concat('app.css'))
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass', function () {
-    gulp.src('./scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./css'));
-});
-
-gulp.task('default', ['sass', 'concat']);
+gulp.task('default', ['clean', 'concat']);
