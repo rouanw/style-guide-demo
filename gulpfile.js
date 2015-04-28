7623dbc9ff4ddd10adbabba6a22fd7e5e0601fa1
Rouan <rmw@thoughtworks.com>
2015-04-28 11:30:55 +0200

    added a sass precompiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('sass', function () {
+    gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
 });
 
+gulp.task('default', ['sass']);
